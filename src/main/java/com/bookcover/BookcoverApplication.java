2057ff5a994391b2a12db79c2a2073053b8e61bc
eunji-97 <eundev.ji@gmail.com>
2025-06-30 21:12:03 +0900

    init project

---
+++ src/main/java/com/bookcover/BookcoverApplication.java
@@ -0,0 +1,13 @@
+package com.bookcover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookcover/BookcoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookcover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
